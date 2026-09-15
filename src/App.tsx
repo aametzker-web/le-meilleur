356d3acb3065ee5ab069d2910ed6f98c4842f2f0
aakhlybov-ux <algebrawhite04@gmail.com>
2026-09-15 18:51:17 +0500

    fix: restore routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,18 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { LanguageProvider } from './context/LanguageContext';
 import { Layout } from './components/layout/Layout';
 import { HomePage } from './pages/Home/HomePage';
+import { NotFoundPage } from './pages/NotFoundPage';
 
 export const App: React.FC = () => {
   return (
     <LanguageProvider>
       <BrowserRouter basename="/le-meilleur">
         <Routes>
-  <Route element={<Layout />}>
-    <Route path="*" element={<HomePage />} />
-  </Route>
-</Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
+        </Routes>
       </BrowserRouter>
     </LanguageProvider>
   );
