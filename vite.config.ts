c6751edf1d13c78407a3bf07ebad3bf59944e6c0
aakhlybov-ux <algebrawhite04@gmail.com>
2026-09-15 20:47:41 +0500

    Prepare project for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/le-meilleur/',
   plugins: [react()],
   resolve: {
     alias: {
